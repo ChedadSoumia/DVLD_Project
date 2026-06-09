01f8d7e684f2e46105861a1ed3449f96b831253d
ChedadSoumia <chedadsoumia4@gmail.com>
2026-06-09 06:02:48 -0700

    Add some changes in Application  DataAccess class and Business class

--- DVDL_business/clsApplication.cs
+++ DVDL_business/clsApplication.cs
@@ -10,38 +10,77 @@ namespace DVDL_business
 {
     public class clsApplication
     {
-        enum enMode { eNewApp = 0, eUpdate = 1}
-        enMode _Mode = enMode.eNewApp;
-
+        enum enMode { eAddNew = 0, eUpdate = 1 }
+        enMode _Mode = enMode.eAddNew;
+        public enum enApplicationType {eNewDrivingLicense= 1, eRenewDrivingLicense= 2 , eReplaceLostDrivingLicense = 3,
+            eReplaceDamagedDrivingLicense = 4, eReleaseDetainedDrivingLicense=5, eNewInternationalLicense=6,eRetakeTest=7
+        };
         public enum enApplicationStatus {eNew=1, eCancelled = 2, eCompleted = 3 };
         public int ApplicationID {  get; set; }
 
-        
-        public clsPerson ApplicantPerson {  get; set; }
+        public int ApplicantPersonID { get; set; }
+
+        public string ApplicantFullName
+        {
+            get
+            {
+                return clsPerson.Find(ApplicantPersonID).FullName;
+            }
+        }
+
+        public clsPerson PersonInfo;
         public DateTime ApplicationDate { get; set; }
-        public enApplicationStatus ApplicationStatus = enApplicationStatus.eNew;
+
+        public int ApplicationTypeID { get; set; }
+        public clsApplicationType ApplicationTypeInfo;
+
+
+        public enApplicationStatus ApplicationStatus { get; set; }
+        
+        public string StatusText
+        {
+            get
+            {
+                switch (ApplicationStatus)
+                {
+                    case enApplicationStatus.eNew:
+                        return "New";
+                    case enApplicationStatus.eCancelled:
+                        return "Cancelled";
+                    case enApplicationStatus.eCompleted:
+                        return "Completed";
+                    default:
+                        return "Unknown";
+                }
+            }
+        }
+        
         public DateTime LastStatusDate { get; set; }
         public float PaidFees { get; set; }
         public int CreatedByUserID {  get; set; }
-
+        public clsUser CreatedByUserInfo;
         public clsApplication()
         {
-            ApplicationID = -1;
-            ApplicantPerson = null;
-            ApplicationDate = DateTime.Now;
-            ApplicationStatus = enApplicationStatus.eNew;
-            LastStatusDate = DateTime.Now;
-            PaidFees = 0;
-            CreatedByUserID = -1;
-
-            _Mode = enMode.eNewApp;
+            this.ApplicationID = -1;
+            this.ApplicantPersonID = -1;
+            this.ApplicationDate = DateTime.Now;
+            this.ApplicationTypeID = -1;
+            this.ApplicationStatus = enApplicationStatus.eNew;
+            this.LastStatusDate = DateTime.Now;
+            this.PaidFees = 0;
+            this.CreatedByUserID = -1;
+            this.CreatedByUserInfo = clsUser.Find(CreatedByUserID);
+            _Mode = enMode.eAddNew;
         }
-        private clsApplication(int applicationID,clsPerson applicantPerson,DateTime applicationDate,
+        private clsApplication(int applicationID,int applicantPersonId,DateTime applicationDate,int applicationTypeID,
             enApplicationStatus applicationStatus,DateTime lastStatusDate,float paidFees,int createdByUserID)
         {
             this.ApplicationID = applicationID;
-            this.ApplicantPerson = applicantPerson;
+            this.ApplicantPersonID = applicantPersonId;
+            PersonInfo = clsPerson.Find(applicantPersonId);
             this.ApplicationDate = applicationDate;
+            this.ApplicationTypeID = applicationTypeID;
+            ApplicationTypeInfo = clsApplicationType.Find(ApplicationTypeID);
             this.ApplicationStatus = applicationStatus;
             this.LastStatusDate = lastStatusDate;
             this.PaidFees = paidFees;
@@ -54,16 +93,17 @@ namespace DVDL_business
         {
          int applicantPersonID = -1;
             DateTime applicationDate = DateTime.Now, LastStatusDate = DateTime.Now;
+            int appliactionTypeID = -1;
             byte applicationSatus = 1;
             float paidFees = 0; 
             int createdByUserID = -1;
 
-            bool IsFound = clsApplicationData.GetApplicationByID(ApplicationId,ref applicantPersonID, ref applicationDate,ref applicationSatus,
+            bool IsFound = clsApplicationData.GetApplicationByID(ApplicationId,ref applicantPersonID, ref applicationDate,ref appliactionTypeID,ref applicationSatus,
                 ref LastStatusDate,ref paidFees,ref createdByUserID);
             if(IsFound)
             {
                 clsPerson applicantPerson = clsPerson.Find(applicantPersonID);
-                return new clsApplication(ApplicationId, applicantPerson, applicationDate, (enApplicationStatus)applicationSatus, LastStatusDate, paidFees, createdByUserID);
+                return new clsApplication(ApplicationId, applicantPersonID, applicationDate, appliactionTypeID,(enApplicationStatus)applicationSatus, LastStatusDate, paidFees, createdByUserID);
             }
             else
             {
@@ -74,7 +114,7 @@ namespace DVDL_business
         }
         private bool _AddNewApplication()
         {
-            ApplicationID = clsApplicationData.AddNewApplication(this.ApplicantPerson.PersonID, this.ApplicationDate,
+            ApplicationID = clsApplicationData.AddNewApplication(this.ApplicantPersonID, this.ApplicationDate,this.ApplicationTypeID,
             (byte)this.ApplicationStatus, this.LastStatusDate, this.PaidFees, this.CreatedByUserID);
 
             return (ApplicationID != -1);
@@ -82,15 +122,30 @@ namespace DVDL_business
         }
         private bool _UpdateApplication()
         { 
-        return clsApplicationData.UpdateApplication(this.ApplicationID,this.ApplicantPerson.PersonID,this.ApplicationDate,
+        return clsApplicationData.UpdateApplication(this.ApplicationID,this.ApplicantPersonID,this.ApplicationDate, this.ApplicationTypeID,
             (byte)this.ApplicationStatus, this.LastStatusDate,this.PaidFees,this.CreatedByUserID);
+        }
+
+        public bool Cancel()
+        {
+            return clsApplicationData.UpdateStatus(this.ApplicationID, 2);
+        }
+
+        public bool SetComplete()
+        {
+            return clsApplicationData.UpdateStatus(this.ApplicationID, 3);
+        }
+
+        public bool Delete()
+        {
+            return clsApplicationData.DeleteApplication(this.ApplicationID);
         }
 
         public bool Save()
         {
             switch (_Mode)
             {
-                case enMode.eNewApp:
+                case enMode.eAddNew:
                     if (_AddNewApplication())
                     {
                         _Mode = enMode.eUpdate;
--- DVLD_DataAccess/clsApplicationData.cs
+++ DVLD_DataAccess/clsApplicationData.cs
@@ -14,7 +14,7 @@ namespace DVLD_DataAccess
 
 
 
-        public static bool GetApplicationByID(int ApplicationID,ref int ApplicantPersonID,ref DateTime ApplicationDate,ref byte AppliactionStatus
+        public static bool GetApplicationByID(int ApplicationID,ref int ApplicantPersonID,ref DateTime ApplicationDate,ref int ApplicationTypeID,ref byte AppliactionStatus
             ,ref DateTime LastStatusDate,ref float PaidFees,ref int CreatedByUser)
         {
 
@@ -40,6 +40,7 @@ namespace DVLD_DataAccess
 
                     ApplicantPersonID = (int)reader["ApplicantPersonID"];
                     ApplicationDate = (DateTime)reader["ApplicationDate"];
+                    ApplicationTypeID = (int)reader["ApplicationTypeID"];
                     AppliactionStatus = (byte)reader["AppliactionStatus"];
                     LastStatusDate = (DateTime)reader["LastStatusDate"];
                     PaidFees = (byte)reader["PaidFees"];
@@ -68,7 +69,7 @@ namespace DVLD_DataAccess
             return isFound;
         }
 
-        public static bool UpdateApplication(int ApplicationID,  int ApplicantPersonID,  DateTime ApplicationDate, byte AppliactionStatus
+        public static bool UpdateApplication(int ApplicationID,  int ApplicantPersonID,  DateTime ApplicationDate, int ApplicationTypeID, byte AppliactionStatus
             ,  DateTime LastStatusDate,  float PaidFees,  int CreatedByUser)
         {
 
@@ -78,16 +79,18 @@ namespace DVLD_DataAccess
             string query = @"Update  Applications  
                             set ApplicantPersonID = @ApplicantPersonID,
                                 ApplicationDate = @ApplicationDate,
+                                ApplicationTypeID = @ApplicationTypeID,
                                 AppliactionStatus = @AppliactionStatus,
                                 LastStatusDate = @LastStatusDate,
                                 PaidFees = @PaidFees,
                                 CreatedByUser = @CreatedByUser
-                                where ApplicationID = @ApplicationID AND ApplicationStatus = 2";
+                                where ApplicationID = @ApplicationID";
 
             SqlCommand command = new SqlCommand(query, connection);
 
             command.Parameters.AddWithValue("@ApplicantPersonID", ApplicantPersonID);
             command.Parameters.AddWithValue("@ApplicationDate", ApplicationDate);
+            command.Parameters.AddWithValue("@ApplicationTypeID", ApplicationTypeID);
             command.Parameters.AddWithValue("@AppliactionStatus", AppliactionStatus);
             command.Parameters.AddWithValue("@LastStatusDate", LastStatusDate);
             command.Parameters.AddWithValue("@PaidFees", PaidFees);
@@ -163,7 +166,7 @@ namespace DVLD_DataAccess
         }
 
 
-        public static int AddNewApplication(int ApplicantPersonID, DateTime ApplicationDate, byte AppliactionStatus
+        public static int AddNewApplication(int ApplicantPersonID, DateTime ApplicationDate,int ApplicatioTypeID, byte AppliactionStatus
             , DateTime LastStatusDate, float PaidFees, int CreatedByUser)
         {
             //this function will return the new person id if succeeded and -1 if not.
@@ -171,8 +174,8 @@ namespace DVLD_DataAccess
 
             SqlConnection connection = new SqlConnection(clsDataAccessSettings.ConnectionString);
 
-            string query = @"INSERT INTO Applicatios (ApplicantPersonID, ApplicationDate, AppliactionStatus,LastStatusDate,PaidFees,CreatedByUser)
-                             VALUES (@ApplicantPersonID, @ApplicationDate, @AppliactionStatus,@LastStatusDate,@PaidFees,@CreatedByUser);
+            string query = @"INSERT INTO Applicatios (ApplicantPersonID, ApplicationDate,ApplicatioTypeID, AppliactionStatus,LastStatusDate,PaidFees,CreatedByUser)
+                             VALUES (@ApplicantPersonID, @ApplicationDate,@ApplicatioTypeID, @AppliactionStatus,@LastStatusDate,@PaidFees,@CreatedByUser);
                              SELECT SCOPE_IDENTITY();";
 
             SqlCommand command = new SqlCommand(query, connection);
@@ -180,6 +183,7 @@ namespace DVLD_DataAccess
             
             command.Parameters.AddWithValue("@ApplicantPersonID", ApplicantPersonID);
             command.Parameters.AddWithValue("@ApplicationDate", ApplicationDate);
+            command.Parameters.AddWithValue("@ApplicatioTypeID", ApplicatioTypeID);
             command.Parameters.AddWithValue("@AppliactionStatus", AppliactionStatus);
             command.Parameters.AddWithValue("@LastStatusDate", LastStatusDate);
             command.Parameters.AddWithValue("@PaidFees", PaidFees);
@@ -209,6 +213,40 @@ namespace DVLD_DataAccess
             }
 
             return ApplicationID;
+        }
+
+        public static bool IsApplicationExist(int ApplicationID)
+        {
+            bool isFound = false;
+
+            SqlConnection connection = new SqlConnection(clsDataAccessSettings.ConnectionString);
+
+            string query = "SELECT Found=1 FROM Applications WHERE ApplicationID = @ApplicationID";
+
+            SqlCommand command = new SqlCommand(query, connection);
+
+            command.Parameters.AddWithValue("@ApplicationID", ApplicationID);
+
+            try
+            {
+                connection.Open();
+                SqlDataReader reader = command.ExecuteReader();
+
+                isFound = reader.HasRows;
+
+                reader.Close();
+            }
+            catch (Exception ex)
+            {
+                //Console.WriteLine("Error: " + ex.Message);
+                isFound = false;
+            }
+            finally
+            {
+                connection.Close();
+            }
+
+            return isFound;
         }
 
         public static bool DeleteApplication(int Application)
@@ -282,6 +320,45 @@ namespace DVLD_DataAccess
             return ApplicationID;
         }
 
+        public static bool UpdateStatus(int ApplicationID,byte NewStatus)
+        {
+
+            int rowsAffected = 0;
+            SqlConnection connection = new SqlConnection(clsDataAccessSettings.ConnectionString);
+
+            string query = @"Update  Applications  
+                            set 
+                                AppliactionStatus = @AppliactionStatus,
+                                LastStatusDate = @LastStatusDate
+                                where ApplicationID = @ApplicationID";
+
+            SqlCommand command = new SqlCommand(query, connection);
+
+            command.Parameters.AddWithValue("@AppliactionStatus", NewStatus);
+            command.Parameters.AddWithValue("@LastStatusDate", DateTime.Now);
+            command.Parameters.AddWithValue("@ApplicationID", ApplicationID);
+
+
+
+            try
+            {
+                connection.Open();
+                rowsAffected = command.ExecuteNonQuery();
+
+            }
+            catch (Exception ex)
+            {
+                //Console.WriteLine("Error: " + ex.Message);
+                return false;
+            }
+
+            finally
+            {
+                connection.Close();
+            }
+
+            return (rowsAffected > 0);
+        }
 
     }
 }
